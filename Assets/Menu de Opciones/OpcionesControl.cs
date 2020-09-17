437373c67ad4a26aecb5d4ee598e07afb5bebeb5
matiasbianco8 <biancomatias8@gmail.com>
2020-09-16 22:24:38 -0300

    Menu de opciones terminado.

--- Assets/Menu de Opciones/OpcionesControl.cs
+++ Assets/Menu de Opciones/OpcionesControl.cs
@@ -74,4 +74,5 @@ public class OpcionesControl : MonoBehaviour
     }
     #endregion
 
+
 }
---
+++ Assets/Sonidos/AudioControl.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioControl : MonoBehaviour
+{
+    public GameObject[] audios; // array de gameobjects
+
+    public GameObject PlayerAll; // objeto del jugador con audios
+
+    bool isMuted;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        isMuted = PlayerPrefs.GetInt("MUTED") == 1;
+
+        audios = GameObject.FindGameObjectsWithTag("AudioSource"); // busca objetos con audio por tags
+
+        PlayerAll = GameObject.FindGameObjectWithTag("PlayerAll"); // busca al jugador por su tag
+
+        for (int i = 0; i < audios.Length; i++)
+        {
+            audios[i].GetComponent<AudioSource>().mute = isMuted;
+        }
+
+
+        PlayerAll.GetComponent<AudioSource>().mute = isMuted;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("m"))
+        {
+            Mute();
+        }
+    }
+
+
+
+    public void Mute()
+    {
+
+
+        isMuted = !isMuted;
+
+
+        for (int i = 0; i < audios.Length; i++)
+        {
+            audios[i].GetComponent<AudioSource>().mute = isMuted;
+
+        }
+
+        PlayerAll.GetComponent<AudioSource>().mute = isMuted;
+
+        PlayerPrefs.SetInt("MUTED", isMuted ? 1 : 0);
+
+    }
+}

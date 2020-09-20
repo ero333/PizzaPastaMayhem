9fda2181ae7f04cd461f5e254ad17a8be142eea0
matiasbianco8 <biancomatias8@gmail.com>
2020-09-20 18:03:17 -0300

    Modificación del codigo del generador de Hordas - Basado en tiempo Creacion de un nuevo codigo generador de Hordas - Basado en la cantidad de enemigos

--- Assets/HordaEnemyGenerator.cs
+++ Assets/HordaEnemyGenerator.cs
@@ -7,6 +7,25 @@ public class HordaEnemyGenerator : MonoBehaviour
     public GameObject enemyPrefab;  //arrastrar el prefab del enemigo aca
 
     public float timer = 4f; // cada cuanto tiempo van a aparecer
+
+
+
+
+    public bool Muchos = false;
+    public float VelocidadDeAparicionHorda = 1; // velocidad en la que aparecen los enemigos
+
+
+
+
+
+    public float DuracionDeHorda = 3f;
+    public float TiempoDeHorda;
+    public float DuracionPausa = 6f;
+
+    public bool HordaOn = false;
+
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -16,18 +35,37 @@ public class HordaEnemyGenerator : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
+        TimerDeHorda();
 
     }
 
     public void CreateEnemy()
     {
+
         Instantiate(enemyPrefab, transform.position, Quaternion.identity); // genera al enemigo
+
     }
 
     public void startGen()
     {
-        InvokeRepeating("CreateEnemy", 0f, timer); // utiliza el metodo de generar enemigos a partir de que tiempo, y cada cuanto tiempo
+
+        if (Muchos == true)
+        {
+            HordaOn = true;
+
+            TiempoDeHorda = DuracionDeHorda;
+        }
+
+        if (HordaOn == true)
+        {
+            InvokeRepeating("CreateEnemy", 0f, VelocidadDeAparicionHorda); // utiliza el metodo de generar enemigos a partir de que tiempo, y cada cuanto tiempo
+        }
+
+
+        if (Muchos == false)
+        {
+            InvokeRepeating("CreateEnemy", 0f, timer); // utiliza el metodo de generar enemigos a partir de que tiempo, y cada cuanto tiempo
+        }
     }
 
     public void stopGen()
@@ -35,4 +73,43 @@ public class HordaEnemyGenerator : MonoBehaviour
         CancelInvoke("CreateEnemy"); // cancela el generar enemigos
 
     }
+
+
+    private void TimerDeHorda()
+    {
+        if (HordaOn == true) // tiempo de spawning
+        {
+
+            if (TiempoDeHorda > 0)
+            {
+                TiempoDeHorda -= Time.deltaTime;
+            }
+            else if (TiempoDeHorda <= 0)
+            {
+                HordaOn = false;
+
+                CancelInvoke("CreateEnemy");
+            }
+
+        }
+
+        if (HordaOn == false) // Tiempo de pausa
+        {
+
+            TiempoDeHorda += Time.deltaTime;
+
+
+            if (TiempoDeHorda > DuracionPausa)
+            {
+                TiempoDeHorda = DuracionDeHorda;
+
+                HordaOn = true;
+
+                InvokeRepeating("CreateEnemy", 0f, VelocidadDeAparicionHorda); // utiliza el metodo de generar enemigos a partir de que tiempo, y cada cuanto tiempo
+            }
+
+        }
+    }
+
+
 }
